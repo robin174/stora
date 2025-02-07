cfa43111fa82f35b29a3af94e9190f53d910e734
Robin Green <robin@designingreen.co.uk>
2025-02-07 10:58:03 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -95,10 +95,10 @@ require_once THEME_DIR_PATH . '/includes/wp-bootstrap-navwalker5.php';
 // Instead of fetching directly via JavaScript, let WordPress provide the API URL.
 function enqueue_custom_scripts() {
     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', [], null, true);
-    wp_enqueue_script('custom-script', get_template_directory_uri() . '/assets/js/drag03-modal.js', ['jquery'], null, true);
+    wp_enqueue_script('drag-modal-script', get_template_directory_uri() . '/js/drag03-modal.js', array('jquery'), time(), true);
 
     // Pass the correct API URL based on the environment
-    wp_localize_script('custom-script', 'wpData', [
+    wp_localize_script('drag-modal-script', 'wpData', [
         'api_url' => AIRTABLE_API_URL
     ]);
 }
